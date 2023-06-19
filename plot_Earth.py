3dd87614c2586d1ce33c57bb9c3b77b8797ced1f
Oana Nica <oana.m.nica@outlook.com>
2023-06-19 15:50:11 -0400

    Added function to project Earth's map onto a sphere

---
+++ plot_Earth.py
@@ -0,0 +1,36 @@
+from mayavi import mlab
+from tvtk.api import tvtk # python wrappers for the C++ vtk ecosystem
+
+
+def plot_Earth(image_file):
+    # create a figure window (and scene)
+    fig = mlab.figure(size=(600, 600))
+    
+    # load and map the texture
+    img = tvtk.JPEGReader()
+    img.file_name = image_file
+    texture = tvtk.Texture(input_connection=img.output_port, interpolate=1)
+    # (interpolate for a less raster appearance when zoomed in)
+
+    # use a TexturedSphereSource, a.k.a. getting our hands dirty
+    R = 6378.14
+    Nrad = 180
+
+    # create the sphere source with a given radius and angular resolution
+    sphere = tvtk.TexturedSphereSource(radius=R, theta_resolution=Nrad,
+                                       phi_resolution=Nrad)
+
+    # assemble rest of the pipeline, assign texture    
+    sphere_mapper = tvtk.PolyDataMapper(input_connection=sphere.output_port)
+    sphere_actor = tvtk.Actor(mapper=sphere_mapper, texture=texture)
+    fig.scene.add_actor(sphere_actor)
+
+    #mlab.plot3d(X_sat, Y_sat, Z_sat, color = (1,1,1), representation = "wireframe")
+    #mlab.quiver3d(np.array([0,0,0]),np.array([0,0,0]),np.array([0,0,0]),np.array([R*2,0,0]),np.array([0,R*2,0]),np.array([0,0,R*2]), line_width = '1', color = (1,1,1))
+
+    
+
+# if __name__ == "__main__":
+#     image_file = "world.topo.bathy.200412.3x5400x2700.jpeg"
+#     plot_Earth(image_file)
+    
--- sso.py
+++ sso.py
@@ -18,6 +18,8 @@ from model_2bp import model_2bp
 from TLE_time2J2000sec import TLE_time2J2000sec
 from kepl2cart import kepl2cart
 import matplotlib.pyplot as plt
+from plot_Earth import plot_Earth
+from mayavi import mlab
 
 
 
@@ -54,61 +56,64 @@ state_0 = np.concatenate((r_vect,v_vect)).flatten()# Initial state
 
 sol = scipy.integrate.odeint(model_2bp,state_0,t_vect) # solving ODE
 
-X_sat = sol[:, 0]  # X-coord [km] in ECI frame over time interval
-Y_sat = sol[:, 1]  # Y-coord [km] in ECI frame over time interval
-Z_sat = sol[:, 2]  # Z-coord [km] in ECI frame over time interval
+X_sat = np.array(sol[:, 0], dtype=float)  # X-coord [km] in ECI frame over time interval
+Y_sat = np.array(sol[:, 1], dtype=float)   # Y-coord [km] in ECI frame over time interval
+Z_sat = np.array(sol[:, 2], dtype=float)  # Z-coord [km] in ECI frame over time interval
 
 # Plot earth + orbit + ECI axes
 
-# Plot spherical Earth
-N = 50
-phi = np.linspace(0, 2 * np.pi, N)
-theta = np.linspace(0, np.pi, N)
-theta, phi = np.meshgrid(theta, phi)
 
-r_Earth = 6378.14  # Average radius of Earth [km]
-X_Earth = r_Earth * np.cos(phi) * np.sin(theta)
-Y_Earth = r_Earth * np.sin(phi) * np.sin(theta)
-Z_Earth = r_Earth * np.cos(theta)
+# # Plot spherical Earth
+# N = 50
+# phi = np.linspace(0, 2 * np.pi, N)
+# theta = np.linspace(0, np.pi, N)
+# theta, phi = np.meshgrid(theta, phi)
 
-# Plotting Earth and Orbit
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.plot_surface(X_Earth, Y_Earth, Z_Earth, color='blue', alpha=0.7)
-ax.plot3D(X_sat, Y_sat, Z_sat, 'black')
-ax.view_init(30, 45)  # Changing viewing angle (adjust as needed)
-plt.title('Two-Body Orbit')
-ax.set_xlabel('X [km]')
-ax.set_ylabel('Y [km]')
-ax.set_zlabel('Z [km]')
+# r_Earth = 6378.14  # Average radius of Earth [km]
+# X_Earth = r_Earth * np.cos(phi) * np.sin(theta)
+# Y_Earth = r_Earth * np.sin(phi) * np.sin(theta)
+# Z_Earth = r_Earth * np.cos(theta)
 
-# Make axes limits
-xyzlim = np.array([ax.get_xlim3d(), ax.get_ylim3d(),      
-                   ax.get_zlim3d()]).T
-XYZlim = np.asarray([min(xyzlim[0]), max(xyzlim[1])])
-ax.set_xlim3d(XYZlim)
-ax.set_ylim3d(XYZlim)
-ax.set_zlim3d(XYZlim)
+# # Plotting Earth and Orbit
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+# ax.plot_surface(X_Earth, Y_Earth, Z_Earth, color='blue', alpha=0.7)
+# ax.plot3D(X_sat, Y_sat, Z_sat, 'black')
+# ax.view_init(30, 45)  # Changing viewing angle (adjust as needed)
+# plt.title('Two-Body Orbit')
+# ax.set_xlabel('X [km]')
+# ax.set_ylabel('Y [km]')
+# ax.set_zlabel('Z [km]')
+
+# # Make axes limits
+# xyzlim = np.array([ax.get_xlim3d(), ax.get_ylim3d(),      
+#                     ax.get_zlim3d()]).T
+# XYZlim = np.asarray([min(xyzlim[0]), max(xyzlim[1])])
+# ax.set_xlim3d(XYZlim)
+# ax.set_ylim3d(XYZlim)
+# ax.set_zlim3d(XYZlim)
 
 
-# Plot ECI axes
-XYZaxes = np.array([[0, 0, 0, r_Earth*2, 0, 0 ], [0, 0, 0, 0, r_Earth*2, 0 ],
-                [0, 0, 0, 0, 0, r_Earth*2 ]])
-X, Y, Z, U, V, W = zip(*XYZaxes)
-ax.quiver(X, Y, Z, U, V, W)
+# # Plot ECI axes
+# XYZaxes = np.array([[0, 0, 0, r_Earth*2, 0, 0 ], [0, 0, 0, 0, r_Earth*2, 0 ],
+#                 [0, 0, 0, 0, 0, r_Earth*2 ]])
+# X, Y, Z, U, V, W = zip(*XYZaxes)
+# ax.quiver(X, Y, Z, U, V, W)
 
-plt.show()
+# %%
+image = "/Users/dylan/Desktop/orbit model/world.topo.bathy.200412.3x5400x2700.jpeg"
+plot_Earth(image)
+mlab.plot3d(X_sat, Y_sat, Z_sat, color = (1,1,1), representation = "wireframe")
+mlab.orientation_axes()
+#plt.show()
 # Plot groundtrack
 
 # Implement orbit precession
+R = 6378.14
+#fig = mlab.figure(size=(600, 600))
 
-
-
-
-
-
-
-
+#mlab.quiver3d(np.array([0,0,0]),np.array([0,0,0]),np.array([0,0,0]),np.array([R*2,0,0]),np.array([0,R*2,0]),np.array([0,0,R*2]), line_width = '1', color = (1,1,1))
+#mlab.quiver3d(np.array([0,0,0]),np.array([R*2,0,0]), line_width = '3', color = (1,1,1))
 
 
 
